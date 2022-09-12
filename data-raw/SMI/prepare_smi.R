8d2fe9d700845aa1fe9086ab89e6e5bc69e348e7
jrenne <jprenne@gmail.com>
2022-09-12 23:46:30 +0200

    update smi

--- data-raw/SMI/prepare_smi.R
+++ data-raw/SMI/prepare_smi.R
@@ -5,4 +5,10 @@ setwd("~/Dropbox/Teaching/MyRpackages/AEC/data-raw/SMI")
 
 smi <- read.csv("SMI.csv")
 
+#smi[,2:7] <- as.numeric(as.matrix(smi[,2:7]))
+
+smi$Date <- as.Date(smi$Date,"%m/%d/%y")
+
 save(smi,file="../../data/smi.rda")
+
+
